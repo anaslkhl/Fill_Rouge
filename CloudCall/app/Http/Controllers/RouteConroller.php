f8ea4f7f9c68efb363bfb5c96500c240236acd96
anaslkhl <anaslkhl123@gmail.com>
2026-02-06 12:18:13 +0100

    creating role and status columns for users table + creating the client table

---
+++ CloudCall/app/Http/Controllers/RouteConroller.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RouteConroller extends Controller
+{
+    //
+
+    public function home()
+    {
+        return view('home');
+    }
+
+    public function form()
+    {
+        return view('form');
+    }
+
+    public function catalog()
+    {
+        return view('catalog');
+    }
+
+    public function product()
+    {
+        return view('product');
+    }
+
+    public function dashboard()
+    {
+        return view('dashboard');
+    }
+
+    public function edit()
+    {
+        return view('edit');
+    }
+}
