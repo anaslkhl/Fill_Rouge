de3ce9f8e94d9a4e35e48a6174048908b6c2fccc
anaslkhl <anaslkhl123@gmail.com>
2026-02-06 12:23:13 +0100

    Creating login controller for signin and signup and logout (Authentication)

--- CloudCall/routes/web.php
+++ CloudCall/routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\RouteConroller;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/catalog', [RouteConroller::class, 'catalog']);
+Route::get('/dashboard', [RouteConroller::class, 'dashboard']);
