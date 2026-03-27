748b3531131547acb6891177fd9d3b22909dc41e
anaslkhl <anaslkhl123@gmail.com>
2026-03-27 15:45:06 +0100

    create the show method that shows the information of user with their call logs

--- CloudCall/app/Http/Controllers/ClientController.php
+++ CloudCall/app/Http/Controllers/ClientController.php
@@ -13,7 +13,7 @@ class ClientController extends Controller
     public function index()
     {
         $clients = Client::latest()->paginate(10);
-        ret
+        return view('client.index', compact('clients'));
     }
 
     public function store(Request $request)
@@ -21,5 +21,18 @@ class ClientController extends Controller
         $data = $request->validated();
 
         $client = Client::create($data);
+    }
+
+    public function show($client)
+    {
+        $client->load('callLogs');
+        return view('client.show', compact('client'));
+    }
+
+
+    public function update(Request $request, Client $client)
+    {
+        $client->update($request->validated());
+        return redirect()->route('client.index')->with('Success', 'Client updated successfully');
     }
 }
