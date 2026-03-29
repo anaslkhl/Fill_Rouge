fb96a279b75abdd06c455b46186373b980ab96af
anaslkhl <anaslkhl123@gmail.com>
2026-03-29 01:11:42 +0100

    create the start call static method startCall to create a call-log

--- CloudCall/app/Models/CallLogs.php
+++ CloudCall/app/Models/CallLogs.php
@@ -18,22 +18,21 @@ class CallLogs extends Model
     public static function startCall(Client $client)
     {
 
-        $agent = User::where('role', 'agent')->wehre('status', 'online')->first();
+        $agent = User::where('role', 'agent')->where('status', 'online')->first();
 
         if (!$agent) {
-            return back()->with('error', 'No agent available');
+            return null;
         }
 
         $agent->status = 'on_call';
         $agent->save();
 
-        $log = CallLogs::create([
+        return self::create([
             'client_id' => $client->id,
             'user_id' => $agent->id,
             'status' => 'calling',
             'created_at' => now()
         ]);
-        return $log;
     }
 
     public function client()
